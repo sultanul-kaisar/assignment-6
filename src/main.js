27d265962e43217fc9e4b7e7b9506236eb2c245e
sultanul kaisar <sultanulkaisar@gmail.com>
2023-08-08 22:31:20 +0600

    Assignment 6

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+import './assets/main.css'
+import './assets/tailwind.css'
+import axios from 'axios'
+
+
+axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.headers['Authorization'] = `Bearer ${localStorage.getItem('token')}`;
+
+const app = createApp(App)
+
+app.use(router)
+
+app.mount('#app')
